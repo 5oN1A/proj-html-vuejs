e03f177b16beffec4106068bf42bebb4639bd603
5oN1A <82112240+5oN1A@users.noreply.github.com>
2021-11-22 15:55:34 +0100

    header component complete

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,16 @@
 <template>
-  <header>
- 
+   <header>
+    <div class="container-fluid d-flex align-items-center justify-content-between">
+      <img class="logo" src="../assets/construction_logo.png" alt="" />
+      <nav>
+        <ul class="d-flex">
+          <li v-for="(item, i) in navItems" :key="i" :class="i === currentActive ? 'active' : ''">
+              <a :href="item.href" >{{item.title}}</a>
+            </li>
+          
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
@@ -9,7 +19,35 @@ export default {
   name: "Header",
   components: {},
   data() {
-    return {};
+    return {
+      navItems: [
+        {
+          title: "HOME",
+          href: "#",
+        },
+        {
+          title: "ABOUT",
+          href: "#",
+        },
+        {
+          title: "SERVICES",
+          href: "#",
+        },
+        {
+          title: "WORK",
+          href: "#",
+        },
+        {
+          title: "ARTICLES",
+          href: "#",
+        },
+        {
+          title: "GET QUOTE",
+          href: "#",
+        },
+      ],
+      currentActive: 0,
+    };
   },
   computed: {},
   methods: {},
