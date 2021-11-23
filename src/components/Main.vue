2fbeb42b27fd73a891757ab189bfec9f55f4d1c0
5oN1A <82112240+5oN1A@users.noreply.github.com>
2021-11-23 15:08:42 +0100

    main layout

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -31,99 +31,410 @@
         <div class="row justify-content-center">
           <div class="col-6">
             <h2 class="pb-4 after_line">Specialists In Modern Construction</h2>
-            <p class="pt-4 lh-lg pb-5">
+            <p class="pt-4 pb-5">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
               ducimus suscipit odio repellat praesentium obcaecati consectetur ,
               voluptatum maiores expedita, saepe pariatur laudantium harum.
             </p>
           </div>
         </div>
-        
-            <div class="row row-cols-4 justify-content-center pb-5">
-              <!-- card -->
-              <div class="col">
-                <div class="block p-3">
-                  <div class="icon_wrapper text-center pb-3"><i class="fa fa-building-o" aria-hidden="true"></i></div>
-                  <h2>Buildings</h2>
-                  <p class="lh-lg">
-                    Lorem ipsum dolor sit amet elit. Eaque deleniti voluptatum
-                    eius adipisci, eius adipisci sequi accusamus.
-                  </p>
-                </div>
-              </div>
-              <!-- card -->
-              <div class="col">
-                <div class="block p-3">
-                  <i class="fa fa-building-o" aria-hidden="true"></i>
-                  <h2>Buildings</h2>
-                  <p class="lh-lg">
-                    Lorem ipsum dolor sit amet elit. Eaque deleniti voluptatum
-                    eius adipisci, sequi accusamus.
-                  </p>
-                </div>
-              </div>
-              <!-- card -->
-              <div class="col">
-                <div class="block p-3">
-                  <i class="fa fa-building-o" aria-hidden="true"></i>
-                  <h2>Buildings</h2>
-                  <p class="lh-lg">
-                    Lorem ipsum dolor sit amet elit. Eaque deleniti voluptatum
-                    eius adipisci, sequi accusamus.
-                  </p>
-                </div>
-              </div>
-              <!-- card -->
-              <div class="col">
-                <div class="block p-3">
-                  <i class="fa fa-building-o" aria-hidden="true"></i>
-                  <h2>Buildings</h2>
-                  <p class="lh-lg">
-                    Lorem ipsum dolor sit amet elit. Eaque deleniti voluptatum
-                    eius adipisci, sequi accusamus.
-                  </p>
-                </div>
-              </div>
 
+        <div class="row row-cols-4 justify-content-center pb-5">
+          <!-- card -->
+          <div class="col">
+            <div class="block p-3">
+              <div class="icon_wrapper text-center pb-3">
+                <i class="fa fa-building-o" aria-hidden="true"></i>
+              </div>
+              <h2>Buildings</h2>
+              <p>
+                Lorem ipsum dolor sit amet elit. Eaque deleniti voluptatum eius
+                adipisci, eius adipisci sequi accusamus.
+              </p>
             </div>
           </div>
-    </section>
-
-    <!-- SECTION 4 -->
-    <section class="section_4 mt-5">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-10">
-            <div class="row row-cols-4">
-
-              <div class="feature text-center">
-                <i class="fa fa-suitcase" aria-hidden="true"></i>
-                <h1>3534</h1>
-                <h6>PLANNING APPLICATIONS</h6>
-              </div>
-              <div class="feature text-center">
-                <i class="fa fa-suitcase" aria-hidden="true"></i>
-                <h1>3534</h1>
-                <h6>PLANNING APPLICATIONS</h6>
-              </div>
-              <div class="feature text-center">
-                <i class="fa fa-suitcase" aria-hidden="true"></i>
-                <h1>3534</h1>
-                <h6>PLANNING APPLICATIONS</h6>
-              </div>
-              <div class="feature text-center">
-                <i class="fa fa-suitcase" aria-hidden="true"></i>
-                <h1>3534</h1>
-                <h6>PLANNING APPLICATIONS</h6>
-              </div>
-
-
+          <!-- card -->
+          <div class="col">
+            <div class="block p-3">
+              <i class="fa fa-building-o" aria-hidden="true"></i>
+              <h2>Buildings</h2>
+              <p>
+                Lorem ipsum dolor sit amet elit. Eaque deleniti voluptatum eius
+                adipisci, sequi accusamus.
+              </p>
+            </div>
+          </div>
+          <!-- card -->
+          <div class="col">
+            <div class="block p-3">
+              <i class="fa fa-building-o" aria-hidden="true"></i>
+              <h2>Buildings</h2>
+              <p>
+                Lorem ipsum dolor sit amet elit. Eaque deleniti voluptatum eius
+                adipisci, sequi accusamus.
+              </p>
+            </div>
+          </div>
+          <!-- card -->
+          <div class="col">
+            <div class="block p-3">
+              <i class="fa fa-building-o" aria-hidden="true"></i>
+              <h2>Buildings</h2>
+              <p>
+                Lorem ipsum dolor sit amet elit. Eaque deleniti voluptatum eius
+                adipisci, sequi accusamus.
+              </p>
             </div>
           </div>
         </div>
       </div>
     </section>
-    <section class="section_5"></section>
+
+    <!-- SECTION 4 -->
+
+    <section class="section_4 mt-5">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-10">
+            <div class="row row-cols-4">
+              <div class="feature text-center">
+                <i class="fa fa-suitcase" aria-hidden="true"></i>
+                <h1>3534</h1>
+                <h6>PLANNING APPLICATIONS</h6>
+              </div>
+              <div class="feature text-center">
+                <i class="fa fa-suitcase" aria-hidden="true"></i>
+                <h1>3534</h1>
+                <h6>PLANNING APPLICATIONS</h6>
+              </div>
+              <div class="feature text-center">
+                <i class="fa fa-suitcase" aria-hidden="true"></i>
+                <h1>3534</h1>
+                <h6>PLANNING APPLICATIONS</h6>
+              </div>
+              <div class="feature text-center">
+                <i class="fa fa-suitcase" aria-hidden="true"></i>
+                <h1>3534</h1>
+                <h6>PLANNING APPLICATIONS</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- SECTION 5 -->
+
+    <section class="section_5">
+      <div class="container text-center">
+        <div class="row justify-content-center">
+          <div class="col-6">
+            <h2 class="pb-4 after_line">Explore recent Work</h2>
+            <p class="pt-4 pb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+              ducimus suscipit odio repellat praesentium obcaecati consectetur ,
+              voluptatum maiores expedita, saepe pariatur laudantium harum.
+            </p>
+          </div>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-8">
+            <div class="row row-cols-3">
+              <div class="col">
+                <div class="ratio ratio-1x1">
+                  <img
+                    src="../assets/project2-featured-15013609-200x200.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div class="col">
+                <div class="ratio ratio-1x1">
+                  <img
+                    src="../assets/project1-featured-294276386-200x200.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div class="col">
+                <div class="ratio ratio-1x1">
+                  <img
+                    src="../assets/project3-featured-189023420-200x200.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <p class="see-more">VIEW ALL PROJECTS</p>
+      </div>
+    </section>
+
+    <!-- SECTION 6 -->
+
+    <section class="section_6">
+      <div class="container text-center">
+        <div class="row justify-content-center">
+          <div class="col-6">
+            <h2 class="pb-4 after_line">Our Core Values</h2>
+            <p class="pt-4 pb-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+              ducimus suscipit odio repellat praesentium obcaecati consectetur ,
+              voluptatum maiores expedita, saepe pariatur laudantium harum.
+            </p>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-8">
+            <div class="row row-cols-4">
+              <!-- QUALITY -->
+              <div class="col">
+                <div class="quality">
+                  <i class="fa fa-home circle-icon" aria-hidden="true"></i>
+                  <h6 class="pt-2 mb-0">Great Services</h6>
+                  <hr class="mb-1" />
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Esse vitae eveniet quis rem ut! Pariatur.
+                  </p>
+                </div>
+              </div>
+              <div class="col">
+                <div class="quality">
+                  <i class="fa fa-home circle-icon" aria-hidden="true"></i>
+                  <h6 class="pt-2 mb-0">Great Services</h6>
+                  <hr class="mb-1" />
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Esse vitae eveniet quis rem ut! Pariatur.
+                  </p>
+                </div>
+              </div>
+              <div class="col">
+                <div class="quality">
+                  <i class="fa fa-home circle-icon" aria-hidden="true"></i>
+                  <h6 class="pt-2 mb-0">Great Services</h6>
+                  <hr class="mb-1" />
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Esse vitae eveniet quis rem ut! Pariatur.
+                  </p>
+                </div>
+              </div>
+              <div class="col">
+                <div class="quality">
+                  <i class="fa fa-home circle-icon" aria-hidden="true"></i>
+                  <h6 class="pt-2 mb-0">Great Services</h6>
+                  <hr class="mb-1" />
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Esse vitae eveniet quis rem ut! Pariatur.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- SECTION 7 -->
+
+    <section class="section_7">
+      <div class="container text-center">
+        <div class="row justify-content-center">
+          <div class="col-6">
+            <h2 class="pb-4 after_line">Our Home Owners Say</h2>
+            <img
+              class="mt-1 round-img"
+              src="../assets/home-testimonial-113165296.jpg"
+              alt="testimonial"
+            />
+            <p class="pt-3">
+              <i>
+                "No man but feels more of a man in the world if he have but a
+                bit of ground that he can call his own. However small it is on
+                the surface, it is four thousand miles deep; and that is a very
+                handsome property.”</i
+              >
+            </p>
+            <p class="pt-4"><b>HARRY SMITH &bull; NEW HOME OWNER</b></p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- SECTION 8 -->
+
+    <section class="section_8">
+      <div class="container text-center">
+        <div class="row justify-content-center">
+          <div class="col-6">
+            <h2 class="pb-4 after_line">Latest News</h2>
+            <p class="pt-4 pb-3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+              ducimus suscipit odio repellat praesentium obcaecati consectetur ,
+              voluptatum maiores expedita, saepe pariatur laudantium harum.
+            </p>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-8">
+            <div class="row row-cols-3 gx-5">
+              <!-- CARD -->
+              <div class="col">
+                <div class="card" style="width: 18rem">
+                  <img
+                    src="..\assets\blog-post-134132600-400x241.jpg"
+                    class="card-img-top"
+                    alt="..."
+                  />
+                  <div class="card-body">
+                    <h3 class="card-title">Card title</h3>
+                    <p class="card-date">December 7th, 2015</p>
+                    <hr />
+                    <p class="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="card" style="width: 18rem">
+                  <img
+                    src="..\assets\blog-post-134132600-400x241.jpg"
+                    class="card-img-top"
+                    alt="..."
+                  />
+                  <div class="card-body">
+                    <h3 class="card-title">Card title</h3>
+                    <p class="card-date">December 7th, 2015</p>
+                    <hr />
+                    <p class="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="card" style="width: 18rem">
+                  <img
+                    src="..\assets\blog-post-134132600-400x241.jpg"
+                    class="card-img-top"
+                    alt="..."
+                  />
+                  <div class="card-body">
+                    <h3 class="card-title">Card title</h3>
+                    <p class="card-date">December 7th, 2015</p>
+                    <hr />
+                    <p class="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <p class="see-more">VIEW ALL ARTICLES</p>
+        </div>
+      </div>
+    </section>
+
+    <!-- SECTION 9-->
+
+    <section class="section_9">
+      <div class="container text-center">
+        <div class="row justify-content-center">
+          <div class="col-6">
+            <h2 class="pb-4 after_line">Trusted Partners</h2>
+            <p class="pt-4 pb-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+              ducimus suscipit odio repellat praesentium obcaecati consectetur ,
+              voluptatum maiores expedita, saepe pariatur laudantium harum.
+            </p>
+          </div>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-8">
+            <div class="row row-cols-5 justify-content-center">
+              <!-- PARTNER -->
+              <div class="col">
+                <div class="partner">
+                  <img
+                    src="..\assets\home-logo11-219096700-320x202.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <!-- PARTNER -->
+              <div class="col">
+                <div class="partner">
+                  <img
+                    src="..\assets\home-logo11-219096700-320x202.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <!-- PARTNER -->
+              <div class="col">
+                <div class="partner">
+                  <img
+                    src="..\assets\home-logo11-219096700-320x202.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <!-- PARTNER -->
+              <div class="col">
+                <div class="partner">
+                  <img
+                    src="..\assets\home-logo11-219096700-320x202.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <!-- PARTNER -->
+              <div class="col">
+                <div class="partner">
+                  <img
+                    src="..\assets\home-logo11-219096700-320x202.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- SECTION 10-->
+
+    <section class="section_10">
+      <div class="container">
+        <div class="row justify-content-center align-items-center">
+          <div class="col-5 text-start">
+            <h2 class="py-4 after_line">Building Inspiring Spaces</h2>
+            <h5 class="pt-4 pb-5 text-start">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+              ducimus suscipit odio repellat praesentium.
+            </h5>
+          </div>
+          <div class="col-3 text-end">
+            <div class="text-center">
+              <a href="#" class="my_btn my_btn_yellow">GET A FREE QUOTE</a>
+              <p class="text-center pt-4">Only takes a few seconds</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
